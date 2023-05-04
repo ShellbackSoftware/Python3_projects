f1618c86cb85c97a728bac9a00aa0fbd0e1cf1f0
ShellbackSoftware <shellbacksoftware@gmail.com>
2023-05-04 12:38:52 -0700

    Added screenshots, GUI tweaks

--- Hangman/README.md
+++ Hangman/README.md
@@ -2,3 +2,17 @@
 Install Python 3, then run <code>python hangmangui.py</code> in the CLI while in the <code>Hangman</code> directory.
 
 Created with tkinter and OOP.
+
+Simple Hangman game with a GUI that pulls a random word from a text file.
+
+Fresh game:
+![New game](images/GUI_newgame.JPG)
+
+Incorrect guess:
+![Wrong letter](images/GUI_wrongletter.JPG)
+
+Did not find the word:
+![Wrong word](images/GUI_wrongword.JPG)
+
+Word was found:
+![Success](images/GUI.JPG)
--- Hangman/hangmangui.py
+++ Hangman/hangmangui.py
@@ -14,7 +14,7 @@ def guess(ev):
     guessEl.delete(0, tk.END)
     resultText.set(hm.checkLetter(letter))
     lblWord.set(hm.printWord())
-    guessesText.set(', '.join(hm.guessed_letters))
+    guessesText.set('Guesses: ' + ', '.join(hm.guessed_letters))
     if hm.guessed_word():
         resultText.set('Congratulations, you guessed "{}" correctly! Press "New Game" to start again!'.format(hm.curWord.capitalize()))
         guessEl.config(state= "disabled")
@@ -24,20 +24,19 @@ def guess(ev):
 
 gui = tk.Tk()
 gui.title('Hangman')
-gui.geometry("1000x500")
+gui.geometry("600x200")
 tk.Label(gui, text="Guess").grid(row=0)
 resultText = tk.StringVar()
-tk.Label(gui, textvariable=resultText).grid(row=2)
-guessesText = tk.StringVar()
-tk.Label(gui, textvariable=guessesText).grid(row=3)
+tk.Label(gui, textvariable=resultText).grid(row=1, columnspan=6)
 guessEl = tk.Entry(gui)
 guessEl.grid(row=0, column=1)
 guessEl.bind('<Return>', guess)
 
-
 lblWord = tk.StringVar()
-tk.Label(gui, textvariable=lblWord,font=('consolas 24 bold')).grid(row=0, column=3 ,columnspan=6,padx=10)
-tk.Button(gui, text="New\nGame", command=lambda:newGame(), font=("Helvetica 10 bold")).grid(row=3, column=8)
+tk.Label(gui, textvariable=lblWord,font=('galliard 24 bold')).grid(row=3,columnspan=6,padx=5)
+guessesText = tk.StringVar()
+tk.Label(gui, textvariable=guessesText).grid(row=2, columnspan=6)
+tk.Button(gui, text="New\nGame", command=lambda:newGame(), font=("Helvetica 10 bold")).grid(row=4)
 
 newGame()
 gui.mainloop()
